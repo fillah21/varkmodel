ac765aae5a8adda682e571ce93088a5c0790e667
fillah21 <fillah.alhaqi11@gmail.com>
2023-05-31 21:10:02 +0700

    selesai seluruh tampilan

---
+++ model/index.php
@@ -0,0 +1,90 @@
+<!doctype html>
+<?php 
+  // if(isset($_COOKIE['v']) || isset($_COOKIE['a']) || isset($_COOKIE['r']) || isset($_COOKIE['k']) || isset($_COOKIE['hasil'])) {
+  //   setcookie('v', '', time()-3600);
+  //   setcookie('a', '', time()-3600);
+  //   setcookie('r', '', time()-3600);
+  //   setcookie('k', '', time()-3600);
+  //   setcookie('hasil', '', time()-3600);
+  // }
+?>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>VARK Test | Data Model</title>
+    <link href="../style.css" rel="stylesheet">
+    <link href="../bootstrap-5.2.0/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="../bootstrap-icons-1.10.3/bootstrap-icons.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.13.4/css/jquery.dataTables.min.css">
+  </head>
+
+  <body class="font-style">
+    <?php 
+      require_once ('../layout/navbar.php');
+    ?>
+
+    <!-- Bagian Content -->
+    <div class="container mt-3">
+        <h3 class="mb-3"><i class="bi bi-clipboard-data-fill"></i> Data Model</h3><hr>
+
+        <div class="d-flex">
+            <div class="card me-5" style="width: 18rem;">
+                <h5 class="ms-3 mt-3 card-title">Jumlah Model</h5>
+                <div class="d-flex justify-content-between align-items-center mx-3">
+                    <h1>2</h1>
+                    <i class="bi bi-clipboard-data-fill" style="font-size: 100px;"></i>
+                </div>
+                <a href="" class="btn btn-primary mx-1 mb-1">Tambah Data Model</a>
+            </div>
+        </div>
+
+        <div class="my-5">
+            <h1 class="mb-3">Data Model</h1>
+            <table id="example" class="display" style="width:100%">
+                <thead>
+                    <tr>
+                        <th>No</th>
+                        <th>Model</th>
+                        <th>Kode</th>
+                        <th>Deskripsi</th>
+                        <th>Aksi</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>1</td>
+                        <td>Visual</td>
+                        <td>V</td>
+                        <td>Visual merupakan ..........................</td>
+                        <td>
+                            <a href="" class="btn btn-success btn-sm">Edit</a>
+                            <a href="" class="btn btn-danger btn-sm">Hapus</a>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>2</td>
+                        <td>Auditory</td>
+                        <td>A</td>
+                        <td>Auditory merupakan ..........................</td>
+                        <td>
+                            <a href="" class="btn btn-success btn-sm">Edit</a>
+                            <a href="" class="btn btn-danger btn-sm">Hapus</a>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+    <!-- Bagian Content Selesai -->
+
+    <script src="../bootstrap-5.2.0/js/bootstrap.bundle.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+    <script src="https://cdn.datatables.net/1.13.4/js/jquery.dataTables.min.js"></script>
+    <script>
+        $(document).ready(function () {
+            $('#example').DataTable();
+        });
+    </script>
+  </body>
+</html>
---
+++ rekomendasi/create.php
@@ -0,0 +1,62 @@
+<!doctype html>
+<?php 
+  // if(isset($_COOKIE['v']) || isset($_COOKIE['a']) || isset($_COOKIE['r']) || isset($_COOKIE['k']) || isset($_COOKIE['hasil'])) {
+  //   setcookie('v', '', time()-3600);
+  //   setcookie('a', '', time()-3600);
+  //   setcookie('r', '', time()-3600);
+  //   setcookie('k', '', time()-3600);
+  //   setcookie('hasil', '', time()-3600);
+  // }
+?>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>VARK Test | Tambah Rekomendasi Belajar</title>
+    <link href="../style.css" rel="stylesheet">
+    <link href="../bootstrap-5.2.0/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="../bootstrap-icons-1.10.3/bootstrap-icons.css">
+  </head>
+
+  <body class="font-style">
+    <?php 
+      require_once ('../layout/navbar.php');
+    ?>
+
+    <!-- Bagian Content -->
+    <div class="container mt-3">
+        <h3><i class="bi bi-grid"></i> Tambah Data Rekomendasi Belajar</h3><hr>
+
+        <form action="">
+            <div class="mb-3 row">
+                <label for="model" class="col-sm-2 col-form-label">Model</label>
+    
+                <div class="col-sm-10">
+                    <select class="form-select" aria-label="Default select example" name="model">
+                        <option selected>Open this select menu</option>
+                        <option value="1">One</option>
+                        <option value="2">Two</option>
+                        <option value="3">Three</option>
+                    </select>
+                </div>
+            </div>
+
+            <div class="mb-3 row">
+                <label for="rekomendasi" class="col-sm-2 col-form-label">Rekomendasi Belajar</label>
+    
+                <div class="col-sm-10">
+                    <textarea class="form-control" id="rekomendasi" name="rekomendasi" rows="10"></textarea>
+                </div>
+            </div>
+    
+            <div class="mt-4">
+                <a href="" class="btn btn-primary me-1">Tambah Data</a>
+                <a href="" class="btn btn-secondary">Kembali</a>
+            </div>
+        </form>
+    </div>
+    <!-- Bagian Content Selesai -->
+
+    <script src="../bootstrap-5.2.0/js/bootstrap.bundle.min.js"></script>
+  </body>
+</html>
